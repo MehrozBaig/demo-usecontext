39d0014a09c35d9a544357c8d03d3984e18b988b
Mehroz Baig <89280407+MehrozBaig@users.noreply.github.com>
2022-07-12 02:45:17 +0500

    update usecontext working with......

--- src/Child.js
+++ src/Child.js
@@ -3,9 +3,11 @@ import ValueContext from "./ValueContext";
 
 function Child() {
     let value = useContext(ValueContext);
+    console.log("value ",value);
     return (
         <div>
-        Child Number {value}
+        Child Number {value[0]}
+        <button onClick={()=> {value[1](++value[0])}}>Update Value </button>
         </div>
     );
 }
